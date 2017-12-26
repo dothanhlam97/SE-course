13b0a257bb53f1d4a6bb1cdbfac34b640c1ea910
Lam Do <dothanhlam.98@gmail.com>
2017-12-27 01:28:27 +0700

    build index

--- testapp/src/main/java/com/test/lam/App.java
+++ testapp/src/main/java/com/test/lam/App.java
@@ -9,9 +9,13 @@ public class App
         //Initialization port SERVER
         setIpAddress("localhost");
         setPort(8080);
+
+        // get(Path.Web.INDEX, IndexController.getIndex());
+
         try {
-            System.out.println("LUL LAM");
-            get("/", (req, res) -> "Hello ahihi");
+            // System.out.println(Path.Web.INDEX);
+            // get("/", (req, res) -> "Hello ahihi");
+            get(Path.Web.INDEX, IndexController.getIndex);
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ testapp/src/main/java/com/test/lam/Path.java
@@ -0,0 +1,11 @@
+package com.test.lam;
+
+public class Path {
+    public static class Web {
+        public static final String INDEX = "/";
+    }
+
+    public static class Template {
+        public final static String INDEX = "velocity/template/index.vm";
+    }
+}
